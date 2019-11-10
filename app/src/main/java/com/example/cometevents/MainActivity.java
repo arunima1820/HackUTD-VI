c55778cb6226fb652fd84f86498ec3d4ed72b44f
freezone2000 <dev576829@gmail.com>
2019-11-09 18:44:59 -0600

    updated

--- app/src/main/java/com/example/cometevents/MainActivity.java
+++ app/src/main/java/com/example/cometevents/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.cometevents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email,password;
+    Button loginButton;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.pass);
+
+        loginButton = findViewById(R.id.loginBtn);
     }
 }
